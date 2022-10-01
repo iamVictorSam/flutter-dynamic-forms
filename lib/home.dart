de6d11efecc557986762883992d4aeee777b1a9b
Iamvictorsam <iamvictorsam@gmail.com>
2022-10-01 20:08:12 +0100

    update+++

--- lib/home.dart
+++ lib/home.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -36,10 +36,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       fontWeight: FontWeight.bold,
                       color: Colors.teal,
                       fontSize: 22)),
-              ...List.generate(_formCount, (index) => _row(index)),
+              const SizedBox(height: 20),
+              ...List.generate(_formCount, (index) => _form(index)),
               const SizedBox(height: 10),
               Text(_result!),
-              const SizedBox(height: 50),
+              const SizedBox(height: 30),
             ],
           ),
         ),
@@ -47,37 +48,31 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _row(int key) {
-    return Row(
-      children: [
-        Text('ID: $key'),
-        const SizedBox(
-          width: 30,
-        ),
-        Expanded(child: TextFormField(
-          onChanged: (val) {
-            _onUpdate(key, val);
-          },
-        )),
-      ],
+  Widget _form(int key) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: TextFormField(
+        decoration: InputDecoration(hintText: 'Form ${key + 1}'),
+        onChanged: (val) => _onUpdate(key, val),
+      ),
     );
   }
 
   _onUpdate(int key, String val) {
     void addJson() {
       Map<String, dynamic> json = {'id': key, 'value': val};
-      _values!.add(json);
+      _values.add(json);
       setState(() {
         _result = _values.toString();
       });
     }
 
-    if (_values!.isEmpty) {
+    if (_values.isEmpty) {
       addJson();
     } else {
-      for (var map in _values!) {
+      for (var map in _values) {
         if (map["id"] == key) {
-          _values![key]['value'] = val;
+          _values[key]['value'] = val;
           setState(() {
             _result = _values.toString();
           });
@@ -85,8 +80,8 @@ class _HomeScreenState extends State<HomeScreen> {
         }
       }
 
-      for (var map in _values!) {
-        for (var map in _values!) {
+      for (var map in _values) {
+        for (var map in _values) {
           if (map["id"] == key) {
             return;
           }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Dynamic Form',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
         useMaterial3: true,
       ),
       home: const HomeScreen(),
